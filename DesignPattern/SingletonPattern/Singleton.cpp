f1657800970d8642b74c629d00013a4e3720127a
mzyKi <2094247798@qq.com>
2024-12-31 18:33:05 +0800

    [Design Pattern] AbstractFactoryPattern,SingletonPattern

---
+++ DesignPattern/SingletonPattern/Singleton.cpp
@@ -0,0 +1,16 @@
+#include "Singleton.h"
+#include <iostream>
+using namespace std;
+Singleton *Singleton::_instance = nullptr;
+Singleton::Singleton()
+{
+    cout << "Singleton...." << endl;
+}
+Singleton *Singleton::Instance()
+{
+    if (_instance == nullptr)
+    {
+        _instance = new Singleton();
+    }
+    return _instance;
+}
